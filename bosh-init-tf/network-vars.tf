ef125469d851864b0d91c9d752500676fc9557e7
Felix Riegger <felix.riegger@sap.com>
2017-01-31 16:24:44 +0100

    Remove unnecessary variable

--- bosh-init-tf/network-vars.tf
+++ bosh-init-tf/network-vars.tf
@@ -4,10 +4,6 @@ variable "ext_net_id" {
   description = "OpenStack external network id to create router interface port"
 }
 
-variable "ext_net_cidr" {
-  description = "OpenStack external network cidr to define ingress security group rules"
-}
-
 variable "availability_zone" {
   description = "OpenStack availability zone name"
 }
