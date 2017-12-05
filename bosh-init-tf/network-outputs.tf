125d4d7a71c0022ec8988f5ead5e9833204ca0e9
David Haines <david.haines@hbgusa.com>
2017-12-05 09:25:01 -0500

    Rename outputs for reuse

--- bosh-init-tf/network-outputs.tf
+++ bosh-init-tf/network-outputs.tf
@@ -1,23 +1,23 @@
-output "network CIDR" {
+output "network_cidr" {
   value = "${openstack_networking_subnet_v2.bosh_subnet.cidr}"
 }
 
-output "network gateway IP" {
+output "network_gateway_ip" {
   value = "${openstack_networking_subnet_v2.bosh_subnet.gateway_ip}"
 }
 
-output "network dns" {
+output "network_dns" {
   value = "[${join(",", openstack_networking_subnet_v2.bosh_subnet.dns_nameservers)}]"
 }
 
-output "network id" {
+output "network_id" {
   value = "${openstack_networking_network_v2.bosh.id}"
 }
 
-output "director private ip" {
+output "director_private_ip" {
   value = "${cidrhost(openstack_networking_subnet_v2.bosh_subnet.cidr, 10)}"
 }
 
-output "router id" {
+output "router_id" {
   value = "${openstack_networking_router_v2.bosh_router.id}"
 }
