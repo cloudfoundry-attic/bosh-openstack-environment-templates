fe20ed0547fcec0a6e9751931ffb4ea869d512ec
Max Becker <max.becker@sap.com>
2017-12-18 13:48:35 +0100

    Changed terraform variable outputs to be better parsable
    - Removed spaces and upper cases in the terraform variable output names
    - Used the same naming as in bosh deployment
    
    This change is based on a pull request by https://github.com/dhaines
    Thanks for your contribution.
    
    [#153593482](https://www.pivotaltracker.com/story/show/153593482)
    
    Signed-off-by: Beyhan Veli <beyhan.veli@sap.com>

--- bosh-init-tf/network-outputs.tf
+++ bosh-init-tf/network-outputs.tf
@@ -1,23 +1,23 @@
-output "network CIDR" {
+output "internal_cidr" {
   value = "${openstack_networking_subnet_v2.bosh_subnet.cidr}"
 }
 
-output "network gateway IP" {
+output "internal_gw" {
   value = "${openstack_networking_subnet_v2.bosh_subnet.gateway_ip}"
 }
 
-output "network dns" {
+output "net_dns" {
   value = "[${join(",", openstack_networking_subnet_v2.bosh_subnet.dns_nameservers)}]"
 }
 
-output "network id" {
+output "net_id" {
   value = "${openstack_networking_network_v2.bosh.id}"
 }
 
-output "director private ip" {
+output "internal_ip" {
   value = "${cidrhost(openstack_networking_subnet_v2.bosh_subnet.cidr, 10)}"
 }
 
-output "router id" {
+output "router_id" {
   value = "${openstack_networking_router_v2.bosh_router.id}"
 }
--- bosh-init-tf/resources-outputs.tf
+++ bosh-init-tf/resources-outputs.tf
@@ -1,7 +1,7 @@
-output "keypair name" {
+output "default_key_name" {
   value = "${openstack_compute_keypair_v2.bosh.name}"
 }
 
-output "allocated floating ip" {
+output "external_ip" {
   value = "${openstack_networking_floatingip_v2.bosh.address}"
 }
